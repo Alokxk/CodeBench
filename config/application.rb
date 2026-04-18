ef9aedc81dd68b27311ebc19c1b28a50d0980e64
Alokxk <alokxk24@gmail.com>
2026-04-19 04:49:01 +0530

    feat: initialized Rails 8.1 API application

---
+++ config/application.rb
@@ -0,0 +1,11 @@
+require_relative "boot"
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module CodeBench
+  class Application < Rails::Application
+    config.load_defaults 8.1
+    config.api_only = true
+  end
+end
